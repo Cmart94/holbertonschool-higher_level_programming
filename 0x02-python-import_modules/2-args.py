3cc2f3f464126fd0802f5376fda11310cefbc1b9
Cmart94 <cristian.313@hotmail.com>
2021-05-06 00:05:37 +0000

    exersice 02

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,6 @@
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
-    
     if argc == 1:
         print("0 arguments.")
     elif argc == 2:
@@ -13,4 +12,3 @@ if __name__ == "__main__":
         print("{:d} arguments:".format(argc - 1))
         for i in range(1, argc):
             print("{:d}: {:s}".format(i, sys.argv[i]))
-
