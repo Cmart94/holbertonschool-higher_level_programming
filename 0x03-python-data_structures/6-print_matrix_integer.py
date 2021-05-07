45737e1f5ac44c9721f8a61404eb8707e9e512e6
Cmart94 <cristian.313@hotmail.com>
2021-05-07 04:47:10 +0000

    exersice 06

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    len_row = len(matrix)
+    if len_row != 0:
+        for i in range(len_row):
+            len_columns = len(matrix[i])
+            for j in range(len_columns):
+                if j < len_columns - 1:
+                    print("{:d}".format(matrix[i][j]), end=' ')
+                else:
+                    print("{:d}".format(matrix[i][j]))
+    else:
+        print('\n')
