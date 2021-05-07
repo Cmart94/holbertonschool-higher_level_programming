c88638df773bb3d489110fc8b35b5c29189353f7
Cmart94 <cristian.313@hotmail.com>
2021-05-07 03:50:17 +0000

    exersice 02

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    list_len = len(my_list)
+    if idx > list_len - 1 or idx < 0:
+        return my_list
+    else:
+        my_list[idx] = element
+        return (my_list)
+
