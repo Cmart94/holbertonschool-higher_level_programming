b97ec85b49efae84e31c60d604da981c167211c7
Cmart94 <cristian.313@hotmail.com>
2021-05-11 03:16:15 +0000

    max integer in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return (None)
+    max_int = 0
+    for i in my_list:
+        if max_int < i:
+            max_int = i
+    return (max_int)
