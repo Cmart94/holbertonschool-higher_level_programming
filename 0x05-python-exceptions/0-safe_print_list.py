32f46d197954d516d0007b80f9314550b1130090
Cmart94 <cristian.313@hotmail.com>
2021-05-17 23:32:21 +0000

    Readme an 00 exersice fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,11 @@
 #/usr/bin/python3
-"""
-functions that prints x elemnt of a list
-"""
 def safe_print_list(my_list=[], x=0):
     list_len = 0
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end='')
             list_len += 1
-        print()
-        return (list_len)
-    except:
-        print()
-        return (list_len)
+    except IndexError:
+        pass
+    print()
+    return (list_len)
