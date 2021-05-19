799823d9982662b2453a896662f36bea60539f5e
Cmart94 <cristian.313@hotmail.com>
2021-05-19 03:10:06 +0000

    02 exersice

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+define the size of a square
+"""
+
+
+class Square:
+    """Private square size validations atribute"""
+    def __init__(self, __size=0):
+        if type(__size) is int:
+            pass
+        else:
+            raise TypeError("size must be an integer")
+        if __size < 0:
+            raise ValueError("size must be >= 0")
+        if __size == 0:
+            self.__size = __size
