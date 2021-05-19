f7f4e5533f8a13bdb55b313b9f690d6bff5aa932
Cmart94 <cristian.313@hotmail.com>
2021-05-19 02:35:12 +0000

    EXERSICE 01, private atribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+define the size of a square
+"""
+
+
+class Square:
+    """Private square size atribute"""
+    def __init__(self, __size=None):
+        self.__size = __size
